33cb9367aea86602aed7a98bd54fc886fd1d31ff
韩文博 <9476400@qq.com>
2019-02-25 00:47:46 +0800

    no message

--- tests/test1.php
+++ tests/test1.php
@@ -1,23 +1,17 @@
 <?php
-require_once '../vendor/autoload.php';
-$test = new \hanwenbo\reflect\Reflect( new Test );
-
-var_dump( $test->getClassReflect()->getName() );
-
-$list = $test->getFunctionListReflect();
-
-foreach( $list as $row ){
-	var_dump( $row->getName() );
-	var_dump( $row->getTitle() );
-}
-
 
 /**
  * Test的标题
  * Class Test
  */
-class Test
+class Test extends TestParents
 {
+	public function __construct()
+	{
+//		var_dump(get_class_methods($this));
+	}
+
+
 	/**
 	 * 列表
 	 */
@@ -42,3 +36,24 @@ class Test
 
 	}
 }
+
+class TestParents{
+	public function pp(){
+
+	}
+}
+require_once '../vendor/autoload.php';
+$test = new \hanwenbo\reflect\Reflect( new Test );
+
+//var_dump( $test->getClassReflect()->getName() );
+
+$list = $test->getFunctionListReflect();
+//
+//foreach( $list as $row ){
+//	var_dump( $row->getName() );
+//	var_dump( $row->getTitle() );
+//}
+
+
+//var_dump(get_class_methods('Test'));
+
